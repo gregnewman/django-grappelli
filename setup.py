db1696efa0894833f644a5d3846892b071cfa7c9
Greg Newman <greg@20seven.org>
2009-12-11 18:59:45 -0500

    changing directory structure for setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""Django Grappelli
+
+A nicer django admin skin.  this is a quick fix setup.py for my virtualenv
+"""
+
+from distutils.core import setup
+
+
+description, long_description = __doc__.split('\n\n', 1)
+VERSION = '0.2.1'
+
+setup(
+    name='grappelli',
+    version=VERSION,
+    author='sehmaschine',
+    description=description,
+    long_description=long_description,
+    platforms=['any'],
+    url='http://github.com/revyver/django-grappelli',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        ],
+    packages=[
+        'grappelli',
+        ],
+    )
