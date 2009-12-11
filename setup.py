f74935e0220160851c7c02c7ac58990ffd59b6bb
Greg Newman <greg@20seven.org>
2009-12-11 05:17:17 -0500

    quickfix setup.py for our virtualenv

---
+++ setup.py
@@ -0,0 +1,33 @@
+"""Django Grappelli
+
+A nicer django admin skin.  this is a quick fix setup.py for my virtualenv
+"""
+
+from distutils.core import setup
+
+
+description, long_description = __doc__.split('\n\n', 1)
+VERSION = '0.2.1'
+
+setup(
+    name='grappelli',
+    version=VERSION,
+    author='sehmaschine',
+    description=description,
+    long_description=long_description,
+    platforms=['any'],
+    url='http://github.com/revyver/django-grappelli',
+    download_url=("http://github.com/revyver/django-grappelli/tarball/a975573cc47677f4f65f75741027bccdc1dac9cf"),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        ],
+    packages=[
+        'grappelli',
+        ],
+    provides=['grappelli'],
+    )
